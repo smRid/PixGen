64dfcc9045cb7c91a92f26800f6b95d6b3c34055
smRid <omorsk911@gmail.com>
2026-04-30 13:02:25 +0600

    implement Better Auth config with MongoDB adapter

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { dash } from "@better-auth/infra";
 import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
@@ -18,4 +19,9 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
         }, 
     },
+  plugins: [
+    dash({
+      apiKey: process.env.BETTER_AUTH_API_KEY,
+    }),
+  ],
 });
