98bfb1fe52f882aa8c49e2ed788e2f084e1db9bb
smRid <omorsk911@gmail.com>
2026-04-30 12:02:26 +0600

    implement user authentication with better-auth

--- src/app/photos/[id]/page.jsx
+++ src/app/photos/[id]/page.jsx
@@ -1,11 +1,41 @@
-"use client";
-import { useParams } from "next/navigation";
+import { Chip } from "@heroui/react";
+import Image from "next/image";
 
-const PhotoDetailsPage = () => {
-    const {id} = useParams();
+const PhotoDetailsPage = async({params}) => {
+    const {id} = await params;
+    const res = await fetch('https://pix-gen-mu.vercel.app/data.json');
+    const allPhotos = await res.json();
+
+    const currentPhoto = allPhotos.find(photo => photo.id == id);
+    console.log(currentPhoto);
+    const {title, imageUrl, prompt, category, model, tags, likes, downloads} = currentPhoto;
     return (
-        <div>
-            Photo Details Page : {id}
+        <div className="mx-auto">
+            <div className="flex gap-4">
+                <div className="relative aspect-auto w-200 h-200">
+                    <Image
+                    src={imageUrl}
+                    alt={title}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    className='object-cover'
+                />
+                </div>
+                <div className="flex flex-col gap-3">
+                    <h1 className="text-3xl">Title: {title}</h1>
+                    <p className="text-2xl">Category: {category}</p>
+                    <span className="text-xl">Model: {model}</span>
+                    <div className="flex">
+                    {
+                        tags.map((tag, index) => <Chip key={index} color="danger" className="mr-2">{tag}</Chip>)
+                    }
+                    </div>
+
+                </div>
+
+            </div>
+            
+             
         </div>
     );
 };
---
+++ src/app/signin/page.jsx
@@ -0,0 +1,110 @@
+"use client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Card,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { Icon } from "@iconify/react";
+import { authClient } from "@/lib/auth-client";
+
+
+export default function SignInPage() {
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const { data, error } = await authClient.signIn.email({
+    email: email, // required
+    password: password, // required
+    rememberMe: true,
+    callbackURL: "/",
+    });
+  };
+
+  const handleGoogleSignIn = async () => {
+    await authClient.signIn.social({
+    provider: "google",
+  });
+  }
+
+  return (
+    <div className="">
+    <Card className="border mx-auto w-180 py-10 mt-4">
+      <h1 className="text-center text-2xl font-bold">Sign In</h1>
+
+      <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+      <div className="w-96 mx-auto">
+        <p className="text-center text-sm text-gray-500 my-4">or</p>
+        <Button onClick={handleGoogleSignIn} variant="tertiary" className="w-full">
+            <Icon icon="devicon:google" />
+            Sign in with Google
+        </Button>
+      </div>
+        
+
+    </Card>
+    </div>
+  );
+}
